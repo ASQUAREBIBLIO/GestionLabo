5de0b7ad4729dfca7e05a4c03baa404f3a918f84
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-06-03 16:47:43 +0000

    modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/0.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/1.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/15.pack 	new file:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/16.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/2.pack modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/3.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/4.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/index.pack modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/index.pack.old 	new file:   .angular/cache/16.0.2/babel-webpack/396c42ae26162c75adc755164fc9c1a19c68cca103a50bbb809b460c7d64d354.json 	new file:   .angular/cache/16.0.2/babel-webpack/673ea36fd3d88eb0406ece58472e2d37e73ba9c722fb615b2df0c74dd60765a9.json 	modified:   src/app/services/projet/projet.service.ts

--- src/app/services/projet/projet.service.ts
+++ src/app/services/projet/projet.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProjet } from 'src/app/models/IProjet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjetService {
+  private membresUrl = 'http://localhost:8082/projets';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllProjets(): Observable<IProjet[]> {
+    return this.http.get<IProjet[]>(this.membresUrl);
+  }
+
+  getProjetById(id: number): Observable<IProjet> {
+    return this.http.get<IProjet>(`${this.membresUrl}/${id}`);
+  }
+
+  createProjet(membre: IProjet): Observable<IProjet> {
+    return this.http.post<IProjet>(this.membresUrl, membre);
+  }
+
+  updateProjet(id: number, membre: IProjet): Observable<IProjet> {
+    return this.http.put<IProjet>(`${this.membresUrl}/${id}`, membre);
+  }
+
+  deleteProjet(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.membresUrl}/${id}`);
+  }
 }
