ae6bd1e5f4d2705f1d677fc53369ebb097551244
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-06-03 16:12:59 +0000

    modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/2.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/3.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/4.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/5.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/index.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/index.pack.old 	new file:   .angular/cache/16.0.2/babel-webpack/85386a51837f7de51f0da059765c08c2083b7832eff80c031b80d366142dbffb.json 	modified:   src/app/membre-list/membre-list.component.html 	modified:   src/app/models/IMembre.ts

--- src/app/models/IMembre.ts
+++ src/app/models/IMembre.ts
@@ -1,9 +1,14 @@
+import { IAdmin } from "./IAdmin";
+import { ILaboratoire } from "./ILaboratoire";
+
 export interface IMembre{
-    id: number;
+    id?: number;
     nom: string;
     prenom: string;
     email: string;
     password: string;
     isDirector: boolean;
     role: any;
+    laboratoire: ILaboratoire;
+    admin: IAdmin;
 }
