28448240a77ca3a158aa84d2180cd281b3c6cd51
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-06-03 17:15:08 +0000

    modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/0.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/2.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/3.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/5.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/6.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/7.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/index.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/index.pack.old 	new file:   .angular/cache/16.0.2/babel-webpack/52ff0ce45fed35741a70a24e8b87fb7a14a095310674af569d67419afd0efc95.json 	new file:   .angular/cache/16.0.2/babel-webpack/5385e5abda83409f7442fb51be646405bf2b2fd4ba19071bd820817ac2de4396.json 	new file:   .angular/cache/16.0.2/babel-webpack/64530aef9f4762f37b387b584c5f2be6149df892b9e050fe36fe38d8e9119efa.json 	new file:   .angular/cache/16.0.2/babel-webpack/ef4d461080b947205f3721ef06a8bbb31a0488c7936768a09b7b6b26281e5454.json 	new file:   .angular/cache/16.0.2/babel-webpack/f5f3929f41cf5225b733b05433d6c584350b0a6c8821811f38fcf090e7dc354f.json 	modified:   src/app/components/uca-rech/uca-rech.component.ts 	modified:   src/app/services/ucaRech/uca-rech.service.ts

--- src/app/services/ucaRech/uca-rech.service.ts
+++ src/app/services/ucaRech/uca-rech.service.ts
@@ -1,33 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IUcaRech } from 'src/app/models/IUcaRech';
+import { IUcaRech } from '../../models/IUcaRech';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UcaRechService {
-  private ucaRechUrl = 'http://localhost:8082/projets';
+  private ucaRechUrl = 'http://localhost:8082/dotations/UcaRech';
 
   constructor(private http: HttpClient) {}
 
-  getAllProjets(): Observable<IUcaRech[]> {
+  getAllUcaRech(): Observable<IUcaRech[]> {
     return this.http.get<IUcaRech[]>(this.ucaRechUrl);
   }
 
-  getProjetById(id: number): Observable<IUcaRech> {
+  getUcaRechById(id: number): Observable<IUcaRech> {
     return this.http.get<IUcaRech>(`${this.ucaRechUrl}/${id}`);
   }
 
-  createProjet(ucaRech: IUcaRech): Observable<IUcaRech> {
+  createUcaRech(ucaRech: IUcaRech): Observable<IUcaRech> {
     return this.http.post<IUcaRech>(this.ucaRechUrl, ucaRech);
   }
 
-  updateProjet(id: number, ucaRech: IUcaRech): Observable<IUcaRech> {
+  updateUcaRech(id: number, ucaRech: IUcaRech): Observable<IUcaRech> {
     return this.http.put<IUcaRech>(`${this.ucaRechUrl}/${id}`, ucaRech);
   }
 
-  deleteProjet(id: number): Observable<void> {
+  deleteUcaRech(id: number): Observable<void> {
     return this.http.delete<void>(`${this.ucaRechUrl}/${id}`);
   }
 }
