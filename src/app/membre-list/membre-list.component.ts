affe2702599c43d0bb64d154363a008f26d2735e
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-06-03 15:27:44 +0000

    modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/0.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/1.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/2.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/3.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/index.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/index.pack.old 	new file:   .angular/cache/16.0.2/babel-webpack/00642b08d61137d04af53a6cf9ac5d8ae953b613e4c1ee932c46ce46f2be0737.json 	new file:   .angular/cache/16.0.2/babel-webpack/c0b734fa778b4043a611b70757470e210e40e0b152c9c6ab22100699e5aff4d1.json 	modified:   src/app/membre-list/membre-list.component.ts

--- src/app/membre-list/membre-list.component.ts
+++ src/app/membre-list/membre-list.component.ts
@@ -8,14 +8,35 @@ import { IMembre } from '../models/IMembre';
   styleUrls: ['./membre-list.component.css']
 })
 export class MembreListComponent implements OnInit {
-    membres: IMembre[] | undefined;
+    membres!: IMembre[];
 
-    constructor(private membreService: MembreService) {
-    }
+    constructor(private membreService: MembreService) { }
 
     ngOnInit() {
-      this.membreService.findAll().subscribe(data => {
-        this.membres = data;
-      });
+      this.getMembres();
+    }
+
+    getMembres() {
+      this.membreService.getAllMembres().subscribe(
+        response => {
+          this.membres = response;
+        },
+        error => {
+          console.log('Error fetching membres:', error);
+        }
+      );
+    }
+
+    deleteMembre(id: number) {
+      if (confirm('Are you sure you want to delete this membre?')) {
+        this.membreService.deleteMembre(id).subscribe(
+          () => {
+            this.getMembres();
+          },
+          error => {
+            console.log('Error deleting membre:', error);
+          }
+        );
+      }
     }
 }
