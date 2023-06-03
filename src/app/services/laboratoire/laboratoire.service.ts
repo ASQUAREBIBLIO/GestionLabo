23367ad2daba2d57fc9ed6b1cb3f6d89206ab354
Fikra434 <oubane.moncif@gmail.com>
2023-06-03 17:34:38 +0100

    laboratoire not tested yet

--- src/app/services/laboratoire/laboratoire.service.ts
+++ src/app/services/laboratoire/laboratoire.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ILaboratoire } from '../../models/ILaboratoire';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LaboratoireService {
+  private baseUrl = 'http://localhost:8082/laboratoires'; // Replace with the actual API URL
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getLaboratoires(): Observable<ILaboratoire[]> {
+    return this.http.get<ILaboratoire[]>(this.baseUrl);
+  }
+
+  getLaboratoireById(laboratoireId: number): Observable<ILaboratoire> {
+    const url = `${this.baseUrl}/${laboratoireId}`;
+    return this.http.get<ILaboratoire>(url);
+  }
+
+  createLaboratoire(laboratoire: ILaboratoire): Observable<any> {
+    return this.http.post(this.baseUrl, laboratoire);
+  }
+
+  updateLaboratoire(laboratoire: ILaboratoire): Observable<any> {
+    const url = `${this.baseUrl}/${laboratoire.id}`;
+    return this.http.put(url, laboratoire);
+  }
+
+  deleteLaboratoire(laboratoireId: number): Observable<any> {
+    const url = `${this.baseUrl}/${laboratoireId}`;
+    return this.http.delete(url);
+  }
 }
