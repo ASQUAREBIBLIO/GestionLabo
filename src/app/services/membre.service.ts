1f3cbe82c74e0f6c1546ae53a6ec2787b528c172
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-06-03 15:10:25 +0000

    modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/0.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/1.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/13.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/index.pack 	modified:   .angular/cache/16.0.2/angular-webpack/6bd894377b5270108a63732e844b4b0fd1195f51/index.pack.old 	new file:   .angular/cache/16.0.2/babel-webpack/343d8ca103fae9c0cc8d9a0bf98bee432e37a4d99e184f29e64d6a0ca9ca09c7.json 	new file:   .angular/cache/16.0.2/babel-webpack/d4e9329223589e6c271112d7cea70ca9a056e4f17c087facb56ebbb8e82d18be.json 	new file:   .angular/cache/16.0.2/babel-webpack/e3c9436ad5d0adf96d82f929318e3c0e9da7e62793c02d725043aa10773f5f52.json 	modified:   src/app/services/membre.service.ts

--- src/app/services/membre.service.ts
+++ src/app/services/membre.service.ts
@@ -3,20 +3,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { IMembre } from '../models/IMembre';
 import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class MembreService {
 
-  private membresUrl: string;
+  private membresUrl = 'http://localhost:8082/membres';
 
-  constructor(private http: HttpClient) {
-    this.membresUrl = 'http://localhost:8082/membres';
-  }
+  constructor(private http: HttpClient) {}
 
-  public findAll(): Observable<IMembre[]> {
+  getAllMembres(): Observable<IMembre[]> {
     return this.http.get<IMembre[]>(this.membresUrl);
   }
 
-  public save(membre: IMembre) {
+  getMembreById(id: number): Observable<IMembre> {
+    return this.http.get<IMembre>(`${this.membresUrl}/${id}`);
+  }
+
+  createMembre(membre: IMembre): Observable<IMembre> {
     return this.http.post<IMembre>(this.membresUrl, membre);
+  }
+
+  updateMembre(id: number, membre: IMembre): Observable<IMembre> {
+    return this.http.put<IMembre>(`${this.membresUrl}/${id}`, membre);
+  }
+
+  deleteMembre(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.membresUrl}/${id}`);
   }
 }
