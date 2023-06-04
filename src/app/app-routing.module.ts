b5192c268f3fdc727fda7d3031726d704924ed32
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-06-04 15:32:27 +0000

    modified:   .angular/cache/16.0.2/angular-webpack/a496eb42392bfcb6a0c3300aa4b69e3391878b9d/0.pack 	new file:   .angular/cache/16.0.2/angular-webpack/a496eb42392bfcb6a0c3300aa4b69e3391878b9d/1.pack 	new file:   .angular/cache/16.0.2/angular-webpack/a496eb42392bfcb6a0c3300aa4b69e3391878b9d/2.pack 	modified:   .angular/cache/16.0.2/angular-webpack/a496eb42392bfcb6a0c3300aa4b69e3391878b9d/index.pack 	new file:   .angular/cache/16.0.2/angular-webpack/a496eb42392bfcb6a0c3300aa4b69e3391878b9d/index.pack.old 	new file:   .angular/cache/16.0.2/babel-webpack/f2cc93e8fb436069b056251fcec96bc324f925b2cc35f57d3a30ecfa69997a26.json 	modified:   src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { MembreListComponent } from './components/membre-list/membre-list.compon
 import { EtablissementComponent } from './components/etablissement/etablissement.component';
 import { LaboratoireComponent } from './components/laboratoire/laboratoire.component';
 import { ResponsableComponent } from './components/responsable/responsable.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 const routes: Routes = [
   {path: 'membres', component: MembreListComponent},
   {path: 'etablissements', component: EtablissementComponent},
   {path: 'laboratoires', component: LaboratoireComponent},
-  {path: 'responsables', component: ResponsableComponent}
+  {path: 'responsables', component: ResponsableComponent},
+  {path: 'admin/dashboard', component: AdminComponent}
 ];
 
 @NgModule({
